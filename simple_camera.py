7b9e60fcd22f8ed1bb24e569ff73da106dcbfdcc
lvanvickle <118330333+lvanvickle@users.noreply.github.com>
2024-04-15 19:49:41 -0400

    Update simple_camera.py
    Added comments for clarity

--- simple_camera.py
+++ simple_camera.py
@@ -1,14 +1,20 @@
 import cv2
-cap = cv2.VideoCapture(0) # 0 is usually the default ID for the first capera.
 
+# Initialize video capture with the default webcam.
+# '0' refers to the first camera connected to the computer.
+cap = cv2.VideoCapture(0)
+
+# Check to see if the camera was initialized successfully.
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
+# Attempt to read frame from camera
 ret, frame = cap.read()
 if not ret:
     print("Can't receive frame (stream end?). Exiting ...")
     cap.release()
     exit()
+# Display the frame
 cv2.imshow('frame', frame)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
