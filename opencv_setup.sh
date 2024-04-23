931ff97ef44c3a2ac374cfc23679838f8f6cecd8
lvanvickle <118330333+lvanvickle@users.noreply.github.com>
2024-04-22 21:06:50 -0400

    Update opencv_setup.sh
    Updated packages

--- opencv_setup.sh
+++ opencv_setup.sh
@@ -7,7 +7,7 @@ sudo apt update && sudo apt upgrade -y
 # Install dependencies for building OpenCV and handling images and video
 echo "Installing necessary libraries..."
 sudo apt install -y cmake build-essential pkg-config git
-sudo apt install -y libjpeg-dev libtiff-dev libjasper-dev libpng-dev
+sudo apt install -y libjpeg-dev libtiff-dev libpng-dev
 sudo apt install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt install -y libxvidcore-dev libx264-dev
 sudo apt install -y libfontconfig1-dev libcairo2-dev
@@ -15,7 +15,7 @@ sudo apt install -y libgdk-pixbuf2.0-dev libpango1.0-dev
 sudo apt install -y libgtk2.0-dev libgtk-3-dev
 sudo apt install -y libatlas-base-dev gfortran
 sudo apt install -y libhdf5-dev libhdf5-103
-sudo apt install -y libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+sudo apt install -y libqt5gui5 libqt5webkit5 libqt5test5 python3-pyqt5
 
 # Install OpenCV Python library
 echo "Installing OpenCV library..."
